35b1ec10e1896ff42bbc14b707b5a6ee965bedea
jckang <jckang568@gmail.com>
2023-11-11 23:13:37 +0900

    Initial commit

---
+++ src/test/java/kr/co/jayce/circuitbreakerpractice/CircuitbreakerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jayce.circuitbreakerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
