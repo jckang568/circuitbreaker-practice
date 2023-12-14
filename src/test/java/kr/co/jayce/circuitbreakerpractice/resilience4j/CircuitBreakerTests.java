6ecfeef1862caec9ffe67adfdf3164c8f66f8ff6
jckang <jckang568@gmail.com>
2023-12-14 18:30:58 +0900

    #Fix: "modify circuitbreaker open test"

--- src/test/java/kr/co/jayce/circuitbreakerpractice/resilience4j/CircuitBreakerTests.java
+++ src/test/java/kr/co/jayce/circuitbreakerpractice/resilience4j/CircuitBreakerTests.java
@@ -19,19 +19,20 @@ class CircuitBreakerTests {
     @Autowired
     private CircuitBreakerService circuitBreakerService;
 
-    private CircuitBreaker circuitBreaker;
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
 
-    @BeforeEach
+    /*@BeforeEach
     void setUp() {
         // CircuitBreakerRegistry에서 CircuitBreaker 가져오기
         CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.ofDefaults();
         this.circuitBreaker = circuitBreakerRegistry.circuitBreaker("circuit-test-5000");
-    }
+    }*/
     @Test
     void testCircuitBreakerSlowCall() throws InterruptedException {
         // slowCallDurationThreshold를 초과하는 시간이 걸리는 시뮬레이션
         List<String> result = new ArrayList<>();
-        for(int i = 0 ; i < 15 ; i ++) {
+        for(int i = 0 ; i < 12 ; i ++) {
             result.add(circuitBreakerService.getCircuitBreaker());
         }
 
@@ -40,7 +41,7 @@ class CircuitBreakerTests {
         // assertEquals("fallbackResult", result); // fallbackMethod에 따라 변경
 
         // CircuitBreaker 상태 확인
-
+        CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("circuit-test-5000");
         assertEquals(CircuitBreaker.State.OPEN, circuitBreaker.getState());
     }
 }
